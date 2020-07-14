24c4a3e398693397caac5962495a5c4b016637c1
James Gill <jagill@fb.com>
2020-07-14 09:12:06 -0400

    Increase RTree construction benchmarks

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,9 +1,66 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use seg_rtree::{Coordinate, Flatbush, Rectangle, SegRTree, LineString};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use seg_rtree::{Coordinate, Flatbush, Rectangle, SegRTree};
 
 use seg_rtree::from_wkt::{parse_wkt, Geometry};
 use std::fs;
 use std::path::Path;
+
+pub fn construction_benchmark(c: &mut Criterion) {
+    let rectangles_list = get_rectangles_list("africa");
+    println!("Benchmarking {} polygons", rectangles_list.len());
+    let mut group = c.benchmark_group("africa");
+
+    for (poly_idx, rectangles) in rectangles_list.iter().enumerate() {
+        println!("Polygon {} has {} segments.", poly_idx, rectangles.len());
+        for degree in [8, 16].iter() {
+            group.bench_with_input(
+                BenchmarkId::new(format!("seg_rtree_inc.{}", poly_idx), degree),
+                degree,
+                |b, &d| {
+                    b.iter(|| {
+                        let mut rtree = SegRTree::new(d, rectangles.len());
+                        for rect in rectangles {
+                            rtree.add(*rect).unwrap();
+                        }
+                    })
+                },
+            );
+            group.bench_with_input(
+                BenchmarkId::new(format!("seg_rtree_bulk.{}", poly_idx), degree),
+                degree,
+                |b, &d| {
+                    b.iter(|| {
+                        SegRTree::new_loaded(d, rectangles);
+                    })
+                },
+            );
+            group.bench_with_input(
+                BenchmarkId::new(format!("flatbush_unsorted.{}", poly_idx), degree),
+                degree,
+                |b, &d| {
+                    b.iter(|| {
+                        Flatbush::new_unsorted(d, rectangles);
+                    })
+                },
+            );
+            group.bench_with_input(
+                BenchmarkId::new(format!("flatbush_sorted.{}", poly_idx), degree),
+                degree,
+                |b, &d| {
+                    b.iter(|| {
+                        Flatbush::new(d, rectangles);
+                    })
+                },
+            );
+        }
+    }
+    group.finish();
+}
+
+criterion_group!(benches, construction_benchmark);
+criterion_main!(benches);
+
+//// Utility functions
 
 fn read_test_case(name: &str) -> Vec<Vec<Geometry>> {
     let filename = format!("benches/testdata/{}.wkt", name);
@@ -36,43 +93,3 @@ fn get_rectangles_list(name: &str) -> Vec<Vec<Rectangle>> {
         .collect();
     rectangles_list
 }
-
-pub fn criterion_benchmark(c: &mut Criterion) {
-    let rectangles_list = get_rectangles_list("africa");
-    let mut group = c.benchmark_group("africa");
-    group.bench_function("seg_rtree_inc", |b| {
-        b.iter(|| {
-            for rectangles in &rectangles_list {
-                let mut rtree = SegRTree::new(8, rectangles.len());
-                for rect in rectangles {
-                    rtree.add(*rect).unwrap();
-                }
-            }
-        })
-    });
-    group.bench_function("seg_rtree_bulk", |b| {
-        b.iter(|| {
-            for rectangles in &rectangles_list {
-                SegRTree::new_loaded(8, rectangles);
-            }
-        })
-    });
-    group.bench_function("flatbush", |b| {
-        b.iter(|| {
-            for rectangles in &rectangles_list {
-                Flatbush::new(8, rectangles);
-            }
-        })
-    });
-    group.bench_function("flatbush_unsorted", |b| {
-        b.iter(|| {
-            for rectangles in &rectangles_list {
-                Flatbush::new_unsorted(8, rectangles);
-            }
-        })
-    });
-    group.finish();
-}
-
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
